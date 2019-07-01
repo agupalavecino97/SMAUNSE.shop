d4903099e97d1d249c10755db7ebcc4e092d9739
Unknown <gonza.pala7@gmail.com>
2019-07-01 15:43:49 -0300

    navigation

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,31 @@
 const express = require('express');
 const router = express.Router();
+const servicio = require('../models/Servicio'); 
+const rubro = require('../models/Rubro');
 
-router.get('/',(req, res) =>{
+router.get('/', async (req, res) =>{
     const mensajes = req.params.mensajes;
-    res.render('index',{mensajes});
+    var todosServicios = await servicio.find({estado:'true'});
+    const rubros = await rubro.find({estado:'true'});
+
+    var servicios = [];
+    todosServicios.forEach(function(servicio, servicioIndex){
+
+        servicios[servicioIndex] = {
+            id: servicio.id,
+            nombre: servicio.nombre,
+            rubros: []
+        }
+        rubros.forEach(rub => {
+            if(servicio.id == rub.id_servicio){
+                servicios[servicioIndex].rubros.push(rub);
+            }
+        });
+         
+    });
+    console.log(servicios);
+    
+    res.render('index',{mensajes, servicios});
 });
 
 router.get('/aboutUs',(req, res) =>{
